c23a8c0a3bae9ac74842cf618578abab114a9fd6
ayushrajgupta1997 <166793229+ayushrajgupta1997@users.noreply.github.com>
2024-04-12 11:19:48 +0000

    Stack_and_Memory._js

---
+++ practicals_js/stack_and_heapMemory.js
@@ -0,0 +1,11 @@
+// How memory works in javascript
+
+/*
+Memory is two types
+Stack Memory and Heap Memory
+
+Stack Uses in Primitive datatypes
+Heap uses in Non Primitive datatypes
+*/
+
+let myChatBotName = "Vigor"
