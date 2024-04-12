5263161c2f68e3a91ea7360a2405f039868cd823
ayushrajgupta1997 <166793229+ayushrajgupta1997@users.noreply.github.com>
2024-04-12 10:15:26 +0000

    05

---
+++ practicals_js/05_comparision.js
@@ -0,0 +1,19 @@
+console.log(5 > 4);  //true
+console.log(5 >= 4); //true
+console.log(5 <= 4); //false
+console.log(5 < 4);  //false
+console.log(5 == 4); //false
+console.log(5 != 4);  //true
+
+//this are normal comparison
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log("3" < 5);  //true
+console.log("03" > 5); //false
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// SO make a rule when u are comparing, making datatype of those values same..
+
+console.log("3" === 5);   //false
